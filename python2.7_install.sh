568a7517deadd8f1ee1c9e61520a714791a80678
xwei <348514942@qq.com>
2017-05-19 18:50:34 +0800

    yum install python-devel

--- python2.7_install.sh
+++ python2.7_install.sh
@@ -10,6 +10,7 @@ wget http://pypi.doubanio.com/packages/87/ba/54197971d107bc06f5f3fbdc0d728a7ae0b
 tar zxf setuptools-27.2.0.tar.gz
 cd setuptools-27.2.0 && python2.7 setup.py install
 python2.7 easy_install.py pip
+yum install python-devel
 mv /usr/bin/python /usr/bin/python.old && ln -s /usr/local/bin/python2.7 /usr/bin/python 
 pip install supervisor
 echo_supervisord_conf > /etc/supervisord.conf
