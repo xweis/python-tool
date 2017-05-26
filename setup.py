f858d75ddcfc004bac1aec0b896f869feaeb3d36
xwei <348514942@qq.com>
2017-05-26 16:54:34 +0800

    更新 setup 添加卸载

--- setup.py
+++ setup.py
@@ -7,19 +7,34 @@ import sys
 import os
 import shutil
 
-installPath = ''
-for path in sys.path:
-    if 'site-packages' in path:
-        installPath = path
+if __name__ == '__main__' :
+    installPath = ''
 
-try :
+    for path in sys.path:
+        if 'site-packages' in path:
+            installPath = path
+            break
+
     installPath = installPath + "/xwLibrary"
-    if os.path.exists(installPath) :
-        #os.rename(installPath,installPath + ".tmp")
-        shutil.rmtree(installPath)
-        shutil.copytree("xwLibrary", installPath)
-    else :
-        shutil.copytree("xwLibrary", installPath)
-    print "\033[0;31;1mxwLibrary\033[0m install Success"
-except Exception,e :
-    print Exception,":",e
+
+    st = "install"
+    if len(sys.argv) > 1 :
+        st = sys.argv[1]
+
+    if st == "install" :
+        try :
+            if os.path.exists(installPath) :
+                shutil.rmtree(installPath)
+                shutil.copytree("xwLibrary", installPath)
+            else :
+                shutil.copytree("xwLibrary", installPath)
+            print "\033[0;31;1mxwLibrary\033[0m install Success"
+        except Exception,e :
+            print Exception,":",e
+    elif st == "uninstall":
+        try :
+            shutil.rmtree(installPath)
+            print "\033[0;31;1mxwLibrary\033[0m uninstall Success"
+        except Exception,e :
+            print Exception,":",e
+
